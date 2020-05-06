b2c6ad67d9860bce3c3574d6e09833584d331edf
Julian542 <waltergc248@gmail.com>
2020-05-06 17:37:01 -0300

    componente titulo

---
+++ src/components/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Title = ({children}) =>(
+    <h1 className="title">{children}</h1>
+)
+
+//Este componente es Stateless y utiliza la prop especial children
